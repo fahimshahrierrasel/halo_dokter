c012974222ae996ff4baa3ab4bba1e6be5447ce5
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-04-14 03:13:57 +0600

    User name and image add in profile section

---
+++ app/src/main/java/com/halodokter/andromeda/halodokter/HaloDokterGlideModule.java
@@ -0,0 +1,8 @@
+package com.halodokter.andromeda.halodokter;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class HaloDokterGlideModule extends AppGlideModule {
+}
