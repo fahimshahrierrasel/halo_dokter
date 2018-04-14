66184bee7d8752b6b1d013a94778367912f1390f
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-04-14 08:14:11 +0600

    layout refactored and list view populated with dummy data

---
+++ app/src/main/java/com/halodokter/andromeda/halodokter/models/DetailsListItem.java
@@ -0,0 +1,13 @@
+package com.halodokter.andromeda.halodokter.models;
+
+public class DetailsListItem extends RecyclerItem {
+    private String buttonText;
+    public DetailsListItem(String itemTitle, String itemDescription, int itemIcon, String buttonText, Class activityClass) {
+        super(itemTitle, itemDescription, itemIcon, activityClass);
+        this.buttonText = buttonText;
+    }
+
+    public String getButtonText() {
+        return buttonText;
+    }
+}
